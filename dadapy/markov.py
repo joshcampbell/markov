b5191004c43f646c69be93d8e47d74b8b5a684ec
Josh Campbell <campbelj@gmail.com>
2015-08-22 23:48:11 -0500

    fix pernicious bugs

--- dadapy/markov.py
+++ dadapy/markov.py
@@ -8,13 +8,13 @@ from Queue import Queue
 from dadapy.cleaner import clean
 
 class MarkovDictionary:
-    """ Manages a nested dict of contextual word occurance probabilities """
-    def __init__(self, source_text=None, depth=2, chunk=5):
+    """ Manages a nested dict of ngramual word occurance probabilities """
+    def __init__(self, source_text=None, depth=3, chunk=5):
       # there must be at least depth + 1 words in the source text
       # depth cannot be less than 2
       self.depth = depth
       self.key_length = self.depth - 1
-      self.contexts = {}
+      self.ngrams = {}
       if source_text != None:
         self.engorge(source_text)
 
@@ -31,42 +31,41 @@ class MarkovDictionary:
           if self.__is_buffer_full(word_buffer):
               self.__upsert_words(word_buffer)
       # the rest of this method is special case handling for the text's end
-      # (it links the first word with the final context)
+      # (it links the first word with the final ngram)
       word_buffer.remove(word_buffer[0])
       word_buffer.append(words[0])
       self.__upsert_words(word_buffer)
 
     def __upsert_words(self, words):
-      context = string.join(words[0:-1])
+      ngram = string.join(words[0:-1])
       word = words[-1]
-      if self.contexts.get(context, None) == None:
-          self.contexts[context] = {}
-      if self.contexts[context].get(word, None) == None:
-          self.contexts[context][word] = 0
-      self.contexts[context][word] += 1
+      if self.ngrams.get(ngram, None) == None:
+          self.ngrams[ngram] = {}
+      if self.ngrams[ngram].get(word, None) == None:
+          self.ngrams[ngram][word] = 0
+      self.ngrams[ngram][word] += 1
 
     def get_lexicon(self):
       words = []
-      for word_dict in self.contexts.values():
+      for word_dict in self.ngrams.values():
         words += word_dict.keys()
       return words
 
-    def __last_context_index(self):
-      return -1 * (self.key_length + 1)
+    def __last_ngram_index(self):
+      return -1 * (self.key_length)
 
-    def __last_context(self, word_list):
-      last_context_index = self.__last_context_index()
-      return word_list[last_context_index:-1]
+    def __last_ngram(self, word_list):
+      last_ngram_index = self.__last_ngram_index()
+      return word_list[last_ngram_index:]
 
     def disgorge(self, length=60):
       # TODO: special case handling for length < depth
-      word_list = random.choice(self.contexts.keys()).split()
+      word_list = random.choice(self.ngrams.keys()).split()
       for i in range(0, length - self.key_length): 
         if(len(word_list) == self.key_length):
             key = string.join(word_list)
         else:
-            key = string.join(self.__last_context(word_list))
-        followers = self.contexts[key]
-        next_word = random.choice(followers.keys())
-        word_list.append(next_word)
+            key = string.join(self.__last_ngram(word_list))
+        followers = self.ngrams[key]
+        word_list.append(random.choice(followers.keys()))
       return string.join(word_list)
